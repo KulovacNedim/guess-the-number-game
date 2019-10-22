701fdbb3097e836e19d7076337dd25ea2d5ae045
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-10-22 16:06:29 +0200

    Project lombok setup

--- console/src/main/java/dev/nedimkulovac/console/Main.java
+++ console/src/main/java/dev/nedimkulovac/console/Main.java
@@ -3,13 +3,13 @@ package dev.nedimkulovac.console;
 import dev.nedimkulovac.AppConfig;
 import dev.nedimkulovac.MessageGenerator;
 import dev.nedimkulovac.NumberGenerator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@Slf4j
 public class Main {
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
+    //private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         log.info("Guess The Number Game");
--- core/src/main/java/dev/nedimkulovac/GameImpl.java
+++ core/src/main/java/dev/nedimkulovac/GameImpl.java
@@ -1,15 +1,13 @@
 package dev.nedimkulovac;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+@Slf4j
 public class GameImpl implements Game {
-
-    private static final Logger log = LoggerFactory.getLogger(Game.class);
 
     @Autowired
     private NumberGenerator numberGenerator;
